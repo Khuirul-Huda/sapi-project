38b0c9f7712c409ff39be3c4e60c35bc7a2ba3f9
Khuirul-Huda <hudah2102@gmail.com>
2024-05-14 09:28:29 +0700

    clean the code

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,22 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-
 </script>
 
 <template>
-  <header class=" container mx-auto mt-5 flex items-center  gap-5 text-white mb-20 outfit-400">
-    <h1 class="outfit-800 text-white text-3xl">SAPI</h1>
-   
-      <nav class="flex gap-14 justify-center w-full ">
-        <RouterLink to="/">Products</RouterLink>
-        <RouterLink to="/">Community</RouterLink>
-        <RouterLink to="/about">Blog</RouterLink>
-        <RouterLink to="/about">Contact</RouterLink>
-      </nav>
-  
+  <header class="container mx-auto mt-5 flex items-center gap-5 text-white mb-20 outfit-400">
+    <h1 class="outfit-800 text-white text-3xl ml-7">SAPI</h1>
+
+    <nav class="flex md:gap-14 gap-3 justify-center w-full">
+      <RouterLink to="/">Products</RouterLink>
+      <RouterLink to="/">Community</RouterLink>
+      <RouterLink to="/about">Blog</RouterLink>
+      <RouterLink to="/about">Contact</RouterLink>
+    </nav>
   </header>
 
   <RouterView />
+
+  <footer class="mt-10">
+    <p class="outfit-300 text-sm  mt-5 md:text-lg text-center text-white">Copyright &copy; 2023.<br> Huda, Sheva, Zuhail, Mahendra, Rizwah</p>
+  </footer>
 </template>
-
-
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,23 +1,38 @@
-<script setup>
-
-</script>
+<script setup></script>
 
 <template>
-  <main class="container mx-auto text-white outfit-800 text-5xl ">
-    <h1 class="text-center">Finds And Keeps Track of Phising</h1>
-    <h1 class="text-center">And Scam Websites</h1>
+  <main class="container mx-auto text-white outfit-800">
+    <h1 class="text-center text-3xl md:text-5xl">Finds And Keeps Track of Phising</h1>
+    <h1 class="text-center text-3xl md:text-5xl">And Scam Websites</h1>
     <div class="flex flex-wrap justify-center">
-      <div class="flex w-1/2 flex-col justify-center">
-        <h1 class="subcolor-one outfit-500 text-3xl">URL Scanner</h1>
+      <div class="flex md:w-1/2 flex-col items-center md:items-start justify-center">
+        <h1 class="subcolor-one outfit-500 text-xl md:text-3xl mt-10 my-5">URL Scanner</h1>
         <form @submit.prevent="submit" class="flex gap-2">
-          <input v-model="url" type="text" class=" text-2xl text-black rounded-lg outfit-300 " name="url" id="url">
-          <input class="cursor-pointer text-lg p-3 bg-subcolor-two rounded-lg outfit-500" type="submit" value="SCAN">
+          <input
+            v-model="url"
+            type="text"
+            class="text-2xl text-black rounded-lg outfit-300"
+            name="url"
+            id="url"
+          />
+          <input
+            class="cursor-pointer text-lg p-3 bg-subcolor-two rounded-lg outfit-500"
+            type="submit"
+            value="SCAN"
+          />
         </form>
-        <p class="outfit-300 text-lg">You can use SAPI to watch for typosquat and lookalike vaariations of a domain and scan dubiouse URLs</p>
-
+        <p class="outfit-300 text-sm w-3/4 md:w-full mt-5 md:text-lg">
+          You can use SAPI to watch for typosquat and lookalike vaariations of a domain and scan
+          dubiouse URLs
+        </p>
       </div>
-      <img src="/src/assets/Other 19.png" width="400px" alt="gambar">
+      <img  src="/src/assets/Other 19.png" text-3xl md:text-5xl alt="gambar" />
     </div>
+
+    <section id="intro" class="mx-auto">
+      <h1 class="text-center text-xl md:text-3xl">What is SAPI?</h1>
+      <p class="outfit-300 text-sm  mt-5 md:text-lg text-center">SAPI stands for Say No To Phish is an application to predict phishing site. It uses Machine Learning Algoritm.</p>
+    </section>
   </main>
 </template>
 
@@ -26,17 +41,18 @@ export default {
   methods: {
     async submit() {
       const urlToCheck = url.value
-      let formData = new FormData();
-formData.append('url', urlToCheck);
+      let formData = new FormData()
+      formData.append('url', urlToCheck)
       const BASE_URL = 'https://sapi-backend.mocci.my.id'
-      fetch(BASE_URL + '/predict' , {body: formData, method: 'POST'}).then((res) => res.json().then((res) => {
-        if (res.predict == 'good') {
-          alert(res.url + ' diprediksi bukan phishing')
-        } else {
-          alert(res.url + ' diprediksi adalah phishing')
-        }
-      }))
-
+      fetch(BASE_URL + '/predict', { body: formData, method: 'POST' }).then((res) =>
+        res.json().then((res) => {
+          if (res.predict == 'good') {
+            alert(res.url + ' diprediksi bukan phishing')
+          } else {
+            alert(res.url + ' diprediksi adalah phishing')
+          }
+        })
+      )
     }
   }
 }
